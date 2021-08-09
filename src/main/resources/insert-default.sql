27372e7ed887f54a10b86acd16d08c3b1c28ac1b
metaprime <metaprime@pc>
2021-08-09 14:46:01 -0300

    Ajustes no ayout e sql para insert default

---
+++ src/main/resources/insert-default.sql
@@ -0,0 +1,4 @@
+insert into eleicao(nome,data_inicio,data_final,versao_objeto) values ('Eleição interna','2021-08-10','2021-08-12',0);
+insert into cargo(nome,versao_objeto) values('Diretor Financeiro (CFO)',0);
+insert into cargo(nome,versao_objeto) values('Desenvolvedor Java',0);
+insert into cargo(nome,versao_objeto) values('Desenvolvedor C++',0);
